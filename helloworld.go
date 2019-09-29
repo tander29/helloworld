3aa21fe05a6941b4f2c086a62385d1a80fdbde6b
Travis Anderson <thetanderson@gmail.com>
2019-09-29 16:52:52 -0400

    import reverse from custom package

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tander29/stringutil"
+)
 
 func main() {
 	fmt.Println("hello world, I can type this in the command line anywhere, thanks goinstall")
+	fmt.Println(stringutil.Reverse("hello world, I can type this in the command line anywhere, thanks goinstall"))
 }
